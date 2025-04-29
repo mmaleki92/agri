37d95e3cd21899eb66b1367398f7a09aeaa44ea2
Morteza <maleki.morteza92@gmail.com>
2025-04-29 19:44:39 +0330

    changed version

--- agri/__init__.py
+++ agri/__init__.py
@@ -5,5 +5,5 @@ Import Python modules directly from GitHub repositories.
 from .auth import authenticate, get_token
 from .repo_manager import import_repo, update_repo, get_repo_structure
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["authenticate", "import_repo", "update_repo"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agri",
-    version="0.1.0",
+    version="0.1.1",
     author="Morteza Maleki",
     author_email="maleki.morteza92@gmail.com",
     description="Anywhere GitHub Repository Importer",
